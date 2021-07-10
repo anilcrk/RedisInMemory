d31f01b63a9634e6a2145e08c07fa931ad32aa74
Anil <anilcirik37@gmail.com>
2021-07-10 19:30:46 +0300

    redis stackexchange implement

---
+++ RedisExchange.Web/Controllers/StringTypeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RedisExchange.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchange.Web.Controllers
+{
+    public class StringTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase _db;
+
+        public StringTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            _db = _redisService.GetDb(0);
+        }
+
+        public IActionResult Index()
+        {
+            
+
+            _db.StringSet("name", "Anıl Cırık");
+            _db.StringSet("ziyaretçi", 100);
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            var value = _db.StringGet("name");
+
+            _db.StringIncrement("ziyaretçi", 1);
+
+            if(value.HasValue)
+            {
+                ViewBag.name = value;
+            }
+
+            return View();
+        }
+    }
+}
---
+++ RedisExchange.Web/Services/RedisService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExchange.Web.Services
+{
+    public class RedisService
+    {
+        private readonly string _redisHost;
+        private readonly string _redisPort;
+        private ConnectionMultiplexer _redis;
+
+        public IDatabase _db { get; set; }
+        public RedisService(IConfiguration configuration)
+        {
+            _redisHost = configuration["Redis:Host"];
+            _redisPort = configuration["Redis:Port"];
+        }
+
+        public void Connect()
+        {
+            var configString = $"{_redisHost}:{_redisPort}";
+
+            // redis ile bağlantı kuruluyor.
+            _redis = ConnectionMultiplexer.Connect(configString);
+
+        }
+
+
+        public IDatabase GetDb(int db)
+        {
+            // db id ye göre geriye db döner
+            return _redis.GetDatabase(db);
+        }
+
+    }
+}
